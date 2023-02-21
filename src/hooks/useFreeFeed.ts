50a0e3542e8f49f1e6601c9b8fdc5bf8c7902a1d
Chris Frewin <frewin.christopher@gmail.com>
2023-02-21 10:51:06 +0100

    dupes fixed once and for all?

--- src/hooks/useFreeFeed.ts
+++ src/hooks/useFreeFeed.ts
@@ -1,27 +1,33 @@
-import { IFeedItem } from './../interfaces/IFeedItem';
-import { useAppDispatch } from './useAppDispatch';
-import { createConsumableQueue } from "./../utils/createConsumableQueue";
-import { useEffect, useRef } from "react";
+import { IFeedItem } from "./../interfaces/IFeedItem";
+import { useAppDispatch } from "./useAppDispatch";
+import {
+  IConsumableQueue,
+  createConsumableQueue,
+} from "./../utils/createConsumableQueue";
+import { useEffect, useState } from "react";
 import * as signalR from "@microsoft/signalr";
 import { useAppSelector } from "./useAppSelector";
-import { appendToItems, setIsConnected, setIsConnecting, setIsError, setVolume } from '../redux/feedSlice';
-import useInterval from './useInterval';
+import {
+  appendToItems,
+  setIsConnected,
+  setIsConnecting,
+  setIsError,
+  setVolume,
+} from "../redux/feedSlice";
+import useInterval from "./useInterval";
 
-export const useFreeFeed = (
-) => {
-  const { isConnecting, isConnected, volume } = useAppSelector(state => state.feed);
-  const dispatch = useAppDispatch();
-  const queueRef = useRef(createConsumableQueue());
-  const connectionRef = useRef(
-    new signalR.HubConnectionBuilder()
-      .withUrl(`${process.env.GATSBY_API_URL}/feed`)
-      .build()
+export const useFreeFeed = () => {
+  const { isConnecting, isConnected, volume } = useAppSelector(
+    (state) => state.feed
   );
+  const dispatch = useAppDispatch();
+  const [queue, setQueue] = useState<IConsumableQueue>();
+  const [connection, setConnection] = useState<signalR.HubConnection>();
 
   const onFreeFeedMessage = (item: IFeedItem) => {
-    console.log("onFreeFeedMessage", item)
+    console.log("onFreeFeedMessage", item);
     dispatch(appendToItems(item));
-    queueRef.current.add({
+    queue?.add({
       sourceType: "base64",
       source: item.mp3data,
       volume,
@@ -35,12 +41,28 @@ export const useFreeFeed = (
     dispatch(setIsConnected(false));
     dispatch(setIsError(false));
     dispatch(setVolume(5));
+    setQueue(createConsumableQueue());
+    setConnection(
+      new signalR.HubConnectionBuilder()
+        .withUrl(`${process.env.GATSBY_API_URL}/feed`)
+        .withAutomaticReconnect()
+        .configureLogging(signalR.LogLevel.Information)
+        .build()
+    );
+    return () => {
+      dispatch(setIsConnecting(false));
+      dispatch(setIsConnected(false));
+      dispatch(setIsError(false));
+      dispatch(setVolume(5));
+      setQueue(undefined);
+      setConnection(undefined);
+    };
   }, []);
 
   useEffect(() => {
     if (isConnecting) {
-      connectionRef.current
-        .start()
+      connection
+        ?.start()
         .then(() => {
           dispatch(setIsConnecting(false));
           dispatch(setIsConnected(true));
@@ -50,26 +72,27 @@ export const useFreeFeed = (
           dispatch(setIsError(true));
           dispatch(setIsConnected(false));
         });
-      connectionRef.current.on("freeFeedMessage", onFreeFeedMessage);
-      connectionRef.current.onclose(() => {
+      connection?.on("freeFeedMessage", onFreeFeedMessage);
+      connection?.onclose(() => {
         dispatch(setIsConnecting(false));
         dispatch(setIsConnected(false));
-      })
+      });
     }
     return () => {
       // connectionRef.current.stop();
-    }
+    };
   }, [isConnecting]);
 
-
   // while we are connected, useInterval to enqueue the advertisement mp3 every 10 minutes
-  useInterval(() => {
-    queueRef.current.add({
-      sourceType: "url",
-      source: '/advertisement.mp3',
-      volume,
-      headline: 'Advertisement',
-    });
-  }, isConnected ? 600000 : null);
+  useInterval(
+    () => {
+      queue?.add({
+        sourceType: "url",
+        source: "/advertisement.mp3",
+        volume,
+        headline: "Advertisement",
+      });
+    },
+    isConnected ? 600000 : null
+  );
 };
-
--- src/utils/createConsumableQueue.ts
+++ src/utils/createConsumableQueue.ts
@@ -3,15 +3,20 @@ import { playBase64StringWithVolume } from "./playBase64StringWithVolume";
 import { playUrlWithVolume } from "./playUrlWithVolume";
 import { sleep } from "./sleep";
 
+export interface IConsumableQueue {
+  add: (item: IFeedData) => void;
+  removeAll: () => void;
+}
+
 export const createConsumableQueue = () => {
-  console.log('creating consuumable queue')
+  console.log("creating consuumable queue");
   const queue: IFeedData[] = [];
   localStorage.setItem("IS_AUDIO_PLAYING", "false");
 
   function add(item: IFeedData) {
     // only add if we are sure the item headline is not already in the queue
     if (queue.find((q) => q.headline === item.headline)) {
-      console.log('already found headline, returning')
+      console.log("already found headline, returning");
       return;
     }
     queue.push(item);
@@ -41,14 +46,11 @@ export const createConsumableQueue = () => {
           processQueue
         );
       }
-    } catch (error) {
-      
-    }
+    } catch (error) {}
     // as latency dump, if we have more than 10 items in the queue, removeAll
     if (queue.length > 10) {
       removeAll();
     }
-    
   }
 
   return { add, removeAll };
